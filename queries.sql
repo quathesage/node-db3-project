c6228dd89e429e9b3283da548027786fae5190f8
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-16 18:00:36 -0600

    queries complete

--- queries.sql
+++ queries.sql
@@ -1,8 +1,16 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT productname, categoryname FROM Products AS p
+	JOIN categories AS c ON c.categoryid = p.categoryid
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT
+    Id, CompanyName
+FROM [order] as o
+JOIN [supplier] as s
+    ON o.ShipVia = s.id
+WHERE OrderDate < "2012-08-09";
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
